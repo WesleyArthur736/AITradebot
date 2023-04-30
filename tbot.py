e26e98b6ba2219062074cf920a73553ea5a98717
FraserP <fraserp@pop-os.localdomain>
2023-04-30 17:20:54 +0800

    ga_python.py is a simple genetic algorithm from the udemy course - linked in the teams chat

--- tbot.py
+++ tbot.py
@@ -34,37 +34,13 @@ import numpy as np
 
 #for val in srsi_values:
 #    print("SRSI", val)
-'''Control the whole process of generting prarmeters and running the bot, storing and evaluating the output'''    
-def main():
-    INIT_MONEY = 100.0
-    SRSI_BUY = 0.2
-    SRSI_SELL= 0.8
-    
-    
-    #initialise data
-    kraken_exchange = ccxt.kraken()
-    kraken_market = kraken_exchange.load_markets()
-    #for mkt in kraken_market:
-        #print(mkt)
-        
-    bitcoin_data = kraken_exchange.fetch_ohlcv("BTC/AUD", timeframe="1d", limit = 720)
-    bitcoin_indicators = pd.DataFrame(bitcoin_data, columns = ["timestamp","open", "high", "low", "close", "volume"])
-    
-    srsi_ind = StochRSIIndicator(bitcoin_indicators['close'])
-    srsi_data= srsi_ind.stochrsi()
-    rsi_ind = RSIIndicator(bitcoin_indicators['close'])
-    rsi_data =rsi_ind.rsi()
 
-    bot = TradeBot(SRSI_BUY, SRSI_SELL, INIT_MONEY,14, 50, 50,bitcoin_indicators, srsi_data )
-    bot.execute_period()
-    print("final dump", bot.cash)
-    
 
 class TradeBot:
     def __init__ (self, SRSI_buy, SRSI_sell, Money, SRSI_window, SRSI_smooth1, SRSI_smooth2, TOHCLV_data, srsi_data):
         print(type(Money))
         self.buy_trigger = SRSI_buy
-        self.sell_trigger =SRSI_sell
+        self.sell_trigger = SRSI_sell
         self.cash = Money
         self.period = SRSI_window
         self.smooth1 = SRSI_smooth1
@@ -76,6 +52,7 @@ class TradeBot:
         print("buy_trig: ", self.buy_trigger)
         print("sell_trig: ", self.sell_trigger)
         print(type(Money))
+
     #def __str__(self):
     #   rep ="Funds at hand: {0}".format(self.cash)
     #    return rep
@@ -90,8 +67,8 @@ class TradeBot:
             print("srsi", srsi_reading, "bot: ",self.cash, " bitcoins: ", self.coins,"\n")
             
             #price_array = self.TOHCLV.to_array()
-            current_price = (self.tohclv['open'] + self.tohclv['close'] + self.tohclv['high'] + self.tohclv['low'])/4
-            #print("current price", current_price[day])
+            current_price_avg = (self.tohclv['open'] + self.tohclv['close'] + self.tohclv['high'] + self.tohclv['low'])/4
+            #print("current price", current_price_avg[day])
             # sell trigger
             if srsi_reading >= self.sell_trigger:
                 sell_trigger_status = True
@@ -102,20 +79,41 @@ class TradeBot:
                 sell_trigger_status = False
             
             if sell_trigger_status == True:
-                self.cash = float(self.cash + (current_price[day] * self.coins))
+                self.cash = float(self.cash + (current_price_avg[day] * self.coins))
                 self.coins = 0
             elif buy_trigger_status == True:
-                self.coins = float(self.coins + (self.cash/ current_price[day]))
-                self.cash = 0 #float((self.cash - (self.cash//current_price[day]) * current_price[day]))
+                self.coins = float(self.coins + (self.cash/ current_price_avg[day]))
+                self.cash = 0 #float((self.cash - (self.cash//current_price_avg[day]) * current_price_avg[day]))
         
         if self.coins > 0:
-            self.cash = float(self.cash + (current_price[day] * self.coins))
+            self.cash = float(self.cash + (current_price_avg[day] * self.coins))
             self.coins = 0
-                
-            
-class EA:
-    pass            
-            
-            
-            
-main()          
+
+
+'''Control the whole process of generting prarmeters and running the bot, storing and evaluating the output'''    
+def main():
+    INIT_MONEY = 100.0
+    SRSI_BUY = 0.2
+    SRSI_SELL= 0.8
+    
+    #initialise data
+    kraken_exchange = ccxt.kraken()
+    kraken_market = kraken_exchange.load_markets()
+    #for mkt in kraken_market:
+        #print(mkt)
+        
+    bitcoin_data = kraken_exchange.fetch_ohlcv("BTC/AUD", timeframe="1d", limit = 720)
+    bitcoin_indicators = pd.DataFrame(bitcoin_data, columns = ["timestamp","open", "high", "low", "close", "volume"])
+    
+    srsi_ind = StochRSIIndicator(bitcoin_indicators['close'])
+    srsi_data = srsi_ind.stochrsi()
+    rsi_ind = RSIIndicator(bitcoin_indicators['close'])
+    rsi_data = rsi_ind.rsi()
+
+    bot = TradeBot(SRSI_BUY, SRSI_SELL, INIT_MONEY, 14, 50, 50, bitcoin_indicators, srsi_data )
+    bot.execute_period()
+    print("final dump", bot.cash)
+
+
+if __name__ == '__main__':
+    main()
