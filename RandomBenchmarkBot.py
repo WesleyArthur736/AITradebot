f752ddd6216a8d391b163297f8d0fb82dd9f9c34
Wesley Arthur <19014181@student.uwa.edu.au>
2023-05-10 07:51:55 +0800

    Finished The random Trading Benchamaking bot. Added commentedout random seed

--- RandomBenchmarkBot.py
+++ RandomBenchmarkBot.py
@@ -22,10 +22,6 @@ def main():
     print(bot_data)
     
     return bot_data
-    
-    
-    
-
 
 class RandomBenchmarkBot:
     def __init__(self):
@@ -35,82 +31,21 @@ class RandomBenchmarkBot:
         
     def get_data(self):
         kraken_exchange = ccxt.kraken()
-        #kraken_market = kraken_exchange.load_markets()
         
         bitcoin_data = kraken_exchange.fetch_ohlcv("BTC/AUD", timeframe="1d", limit = 720)
         bitcoin_indicators = pd.DataFrame(bitcoin_data, columns = ["timestamp","open", "high", "low", "close", "volume"])
         
         return bitcoin_indicators
-        
-    def get_triggers(self):
-        
-        sell_triggers = []
-        buy_triggers = []
-        sosc= self.stochOsc.to_numpy()
-        so_s= self.so_signal.to_numpy()
-        
-        # prepare arrays to give point in ranges of significance to the Stochastic Oscillator index
-        so_overbought = np.argwhere(sosc>80)
-        so_oversold = np.argwhere(sosc<20)
-        
-        so_crossover_sig = np.argwhere(np.greater(so_s , sosc))
-        crossover_back = np.argwhere(np.greater(sosc , so_s))
-        crossover_booleans_osc = np.in1d(crossover_back, so_crossover_sig + 1 )
-        crossover_booleans_sig= np.in1d(so_crossover_sig,crossover_back +1)
-        
-        # Determine the triggers for selling
-        # Output a list of selling triggers
-        for inx in so_overbought: 
-            if np.any(crossover_back == inx) == True: 
-                idx_crossover = np.where(crossover_back == inx) #suspect contradiction
-                print(idx_crossover[0], inx)
-                
-                if crossover_booleans_osc[idx_crossover[0]] == True:
-                    print("i made it to here")
-                    if so_s[inx] < sosc[inx]: 
-                        #print("loading variable")
-                        sell_triggers.append((inx[0], "-1"))
-        print(sell_triggers)
-        
-        # Determine the triggers for buying
-        for inx in so_oversold: 
-            if np.any(so_crossover_sig == inx) == True:
-                idx_crossover = np.where(so_crossover_sig == inx)
-                
-                if crossover_booleans_sig[idx_crossover[0]] == True:
-                    if so_s[inx] > sosc[inx]:
-                        buy_triggers.append((inx[0], 1))
-                    
-        print (buy_triggers)      
-        
-        return sell_triggers,buy_triggers 
-    
-        
-    
-    def formulate_orders(self, sell_triggers,buy_triggers):
-        NO_DAYS_TRADE = 719
-        orders = np.zeros((NO_DAYS_TRADE))
-        for tup in buy_triggers:
-            if tup[0] < NO_DAYS_TRADE:
-                
-                print("tup",tup[0])
-            
-                orders[tup[0]]=tup[1]
-                
-        for tup in sell_triggers:
-            if tup[0] < NO_DAYS_TRADE:
-                orders[tup[0]]=tup[1]
-        
-        return pd.Series(orders.astype(int), name ="trade_signal")
     
     def execute_trades(self, trade_signals, fee_percentage):
         portfolio_value=[]
         sell_signal = []
         buy_signal = []
+        np.random.seed(1)
         
         ndo = trade_signals['open'][1:720]
         FREQUENCY_OF_TRADE = 0.01
-        buy = True # set buy=True - next tome buy, buy=False - next time sell
+        buy = True # set buy=True - next time buy, buy=False - next time sell
         
         for day in range(len(ndo)):
             trade_prob =  np.random.random()   
